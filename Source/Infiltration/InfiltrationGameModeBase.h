43b04211caeee45bbc436ebd6db1394eed51e69b
Christophe Fracheboud <cfracheboud@gaming.tech>
2021-10-07 12:27:35 +0200

    Create project

---
+++ Source/Infiltration/InfiltrationGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "InfiltrationGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class INFILTRATION_API AInfiltrationGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
