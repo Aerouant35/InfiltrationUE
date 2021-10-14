dab0686b92c02ae6eb88f1dd39b15e3f6cab6900
ErwanE <erwan-eps@orange.fr>
2021-10-14 17:10:21 +0200

    Add Player UI, Victory screen and Defeat screen

---
+++ Source/Infiltration/Private/UI/PlayerWidget.cpp
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UI/PlayerWidget.h"
+
+UPlayerWidget::UPlayerWidget(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
+{
+}
+
+void UPlayerWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+}
+
+void UPlayerWidget::UpdateProgressBarPercent(const float Percent) const
+{
+	FoodProgress->SetPercent(Percent);
+}
---
+++ Source/Infiltration/Public/UI/PlayerWidget.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "Components/ProgressBar.h"
+#include "PlayerWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class INFILTRATION_API UPlayerWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, meta = (BindWidget))
+	class UProgressBar* FoodProgress;
+	
+public:
+	UPlayerWidget(const FObjectInitializer& ObjectInitializer);
+
+private:
+	virtual void NativeConstruct() override;
+
+public:
+	/** Update progress bar percent in Player UI.
+	 * @param	Percent					Float range between 0 and 1.
+	 */
+	void UpdateProgressBarPercent(const float Percent) const;
+};
