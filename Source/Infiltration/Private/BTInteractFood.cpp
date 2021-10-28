cdd5bbc760a9282211cbe5bc4f2d077e85c186c5
Luxifer696 <38383550+Luxifer696@users.noreply.github.com>
2021-10-28 11:49:22 +0200

    Debug Pickup AI
    L'IA peut à nouveau reprendre sa food

--- Source/Infiltration/Private/BTInteractFood.cpp
+++ Source/Infiltration/Private/BTInteractFood.cpp
@@ -12,7 +12,7 @@ EBTNodeResult::Type UBTInteractFood::ExecuteTask(UBehaviorTreeComponent & OwnerC
 		// Dépose la nourriture si l'IA en possède et si le spot n'en possède pas
 		// Pour une raison qui m'échappe, les foodSpot Overlap deux fois pour chaque fruit
 		// Donc une valeur de 2 pour NumberOfFoods correspond en réalité à un seul fruit
-		if(AICon->GetAICharacter()->GetHasFood() && AICon->GetCurrentSpot()->GetNumberOfFoods() < 3)
+		if(AICon->GetAICharacter()->GetHasFood() || AICon->GetAICharacter()->GetHasDropFood() && AICon->GetCurrentSpot()->GetNumberOfFoods() < 3)
 			{
 			//BlackboardComp = AICon->GetBlackboardComp();
 
